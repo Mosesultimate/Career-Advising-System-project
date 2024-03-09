090ab5f878abbdabd9899ab27d80ae56a3ce998b
Moses Matola <135230097+Mosesultimate@users.noreply.github.com>
2024-03-09 13:28:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
-from langchain.llms import OpenAI
+from langchain_community.llms import OpenAI
 
 import os
 
