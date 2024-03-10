223fa25cf6753ed1f76fedc018b42553c77c8e49
Moses Matola <135230097+Mosesultimate@users.noreply.github.com>
2024-03-10 14:12:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain_community.llms import OpenAI
 
-import os
 
-
-os.environ["OPENAI_API_KEY"] ="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"
 
 
 st.title("Career-Advising System")
@@ -25,7 +22,7 @@ if submit:
     if skills and interests and goals and industry and academic_background:
          prompt = PromptTemplate(input_variables=["interests","skills","goals","industry"], template="Recommend career for someone who {goals} goals,{skills} skills,{interests} interessts and prefers to work in {industry} industry and has {academic_background} as academic background.Mention just the career,do not explain")
 
-         llm=OpenAI(temperature=0.9)
+         llm=OpenAI(temperature=0.9,apikey="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")
          llm_chain = LLMChain(prompt=prompt, llm=llm)
          variables={"interests":interests,"skills":skills,"goals":goals,"industry":industry,"academic_background":academic_background}
          response=llm_chain.run(variables)
