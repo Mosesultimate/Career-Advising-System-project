cb2d0cd7da28df5048d274ce64fc273177f6055b
Moses Matola <135230097+Mosesultimate@users.noreply.github.com>
2024-03-10 17:04:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,36 @@ from langchain.llms import OpenAI
 from supabase import create_client
 
 
+import base64
+import streamlit as st
+
+
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+set_background('./careers.jpg')
+
 SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNraG9iYnR5ZmpjaGdmemJnYW10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTAwNzA2ODIsImV4cCI6MjAyNTY0NjY4Mn0.KwHdoe8b_4YzJeVFoJbEE38m-Cb3H6juBZDEbt5cdY4"
 SUPABASE_URL="https://ckhobbtyfjchgfzbgamt.supabase.co"
 supabase=create_client(SUPABASE_URL,SUPABASE_KEY)
 
 data=supabase.table("career").select("*").execute()
-print("Data in the career table is")
-print("---------")
 print(data)
 
 st.title("Career-Advising System")
