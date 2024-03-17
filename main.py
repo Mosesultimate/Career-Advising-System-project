6d9d28c403b97e3f82455daa38c8629ada97b375
Moses Matola <135230097+Mosesultimate@users.noreply.github.com>
2024-03-18 00:13:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from langchain.llms import OpenAI
 from supabase import create_client
 import re
 import base64
-import streamlit as st
+from langchain.llms import HuggingFaceHub
+import os
+
+os.environ["HUGGINGFACEHUB_API_TOKEN"]="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"
+
+
 
 
 def get_base64(bin_file):
@@ -68,7 +73,8 @@ def create_app(openai_api_key,supabase):
 
                 prompt = PromptTemplate(input_variables=["interests","skills","goals","industry"], template="Recommend career for someone who {goals} goals,{skills} skills,{interests} interessts and prefers to work in {industry} industry and has {academic_background} as academic background.Mention just the career,do not explain")
 
-                llm=OpenAI(temperature=0.9,openai_api_key=openai_api_key)
+                #llm=OpenAI(temperature=0.9,openai_api_key=openai_api_key)
+                llm = HuggingFaceHub( repo_id="mistralai/Mixtral-8x7B-Instruct-v0.1")
                 llm_chain = LLMChain(prompt=prompt, llm=llm)
                 variables={"interests":interests,"skills":skills,"goals":goals,"industry":industry,"academic_background":academic_background}
                 response=llm_chain.run(variables)
